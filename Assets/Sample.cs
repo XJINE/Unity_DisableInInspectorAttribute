d34cc78fab65ade8b8a3c2e6eefb89b91e1ecb8c
XJINE <xjine.andorelseif@gmail.com>
2018-11-06 16:31:50 +0900

    First commit.

---
+++ Assets/Sample.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Sample : MonoBehaviour
+{
+    // NOTE:
+    // DisableInInspector is ignored when any other GUI attribute is set after it.
+    // DisableInInspector can not limit array or list size changing.
+
+    #region Field
+
+    [DisableInInspector]
+    public int intValue = 0;
+
+    [Range(0, 3), DisableInInspector]
+    public int intRange = 0;
+
+    [DisableInInspector, Range(0, 3)]
+    public int intRangeR = 0;
+
+    [DisableInInspector]
+    public int[] intArray;
+
+    [DisableInInspector]
+    public List<int> intList;
+
+    [HideInInspector, DisableInInspector]
+    public int intHideIn;
+
+    [DisableInInspector, HideInInspector]
+    public int intHideInR;
+
+    [SerializeField, DisableInInspector]
+    private int intSerialize;
+
+    [DisableInInspector, SerializeField]
+    private int intSerializeR;
+
+    #endregion Field
+}
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# DisableInInspectorAttribute
-Add [DisableInInspector] attribute to make uneditable field.
+# Unity_DisableInInspectorAttribute
+
+<img src="https://github.com/XJINE/Unity_DisableInInspectorAttribute/blob/master/Screenshot.png" width="100%" height="auto" />
+
+Add ``[DisableInInspector]`` attribute to make uneditable field.
+
+## Import to Your Project
+
+You can import this asset from UnityPackage.
+
+- [DisableInInspectorAttribute.unitypackage](https://github.com/XJINE/Unity_DisableInInspectorAttribute/blob/master/DisableInInspectorAttribute.unitypackage)
+
+## How to Use
+
+Set ``[DisableInInspector]`` attribute on your field.
+
+## Limitation
+
+``[DisableInInspector]`` is ignored when any other GUI attribute is set after it.
+
+``[DisableInInspector]`` can not limits array or list size changing.
